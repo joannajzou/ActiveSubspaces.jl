87791fc925c1c7e58d633d58e5c3c607229f1397
joannajzou <joannajzou@gmail.com>
2023-03-06 16:39:11 -0500

    jjz - add subspaces test

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,5 +1,5 @@
 using SafeTestsets
 
-@safetestset "Gibbs Distribution Tests" begin include("distributions_tests.jl") end
+@safetestset "Gibbs Tests" begin include("distributions_tests.jl") end
 @safetestset "QoI Tests" begin include("qoi_tests.jl") end
-
+@safetestset "Subspaces Tests" begin include("subspaces_tests.jl") end
