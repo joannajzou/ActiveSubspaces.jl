e9bb44580d91bb8c952eb9ebe3cdd86f790c633f
joannajzou <joannajzou@gmail.com>
2023-07-05 16:12:06 -0700

    refactor doublewell1D code

--- examples/doublewell1D/model_param1.jl
+++ examples/doublewell1D/model_param1.jl
@@ -1,4 +1,6 @@
-# specify potential function and its gradients
+### script for specifying model variables and integrators 
+
+# specify potential function and its gradients ###############################################################
 V(x, θ) = (θ[1] * x.^2) / 2 - (θ[2] * x.^4) / 4 - 1 # with neg sign
 ∇xV(x, θ) = θ[1] * x - θ[2] * x.^3
 ∇θV(x, θ) = [x^2 / 2, -x^4 / 4]
@@ -7,10 +9,35 @@ V(x, θ) = (θ[1] * x.^2) / 2 - (θ[2] * x.^4) / 4 - 1 # with neg sign
 πgibbs0 = Gibbs(V=V, ∇xV=∇xV, ∇θV=∇θV)
 πgibbs = Gibbs(πgibbs0, β=1.0)
 
-# define sampling density
-μθ = 4*ones(2)
+# define sampling density for θ
+μθ = 4*ones(2) 
 Σθ = 0.2*I(2) 
-ρθ = MvNormal(μθ, Σθ) # prior on θ 
-θrng = [1.8, 6.2] # [μθ[1] - 3.5*sqrt(Σθ[1,1]), μθ[1] + 3.5*sqrt(Σθ[1,1])]
+ρθ = MvNormal(μθ, Σθ)                           # prior on θ 
+θrng = [μθ[1] - 3.5*sqrt(Σθ[1,1]), μθ[1] + 3.5*sqrt(Σθ[1,1])] # θ-domain
 
-ρx0 = Uniform(-2, 2) # for initial state x0
+# define x-domain
+ρx0 = Uniform(-2, 2)                            # sampling density for initial state x0
+ll = -3; ul = 3                                 # lower and upper limits
+xplot = Vector(LinRange(ll, ul, 1000))          # plot domain
+
+
+# define integrators #########################################################################################
+
+# QuadIntegrator
+ngrid = 100                                     # number of θ quadrature points in 1D
+ξθ, wθ = gausslegendre(ngrid, θrng[1], θrng[2]) # 2D quad points over θ
+ξx, wx = gausslegendre(200, -10, 10)            # 1D quad points over x
+GQint = GaussQuadrature(ξx, wx)
+
+# MCIntegrator
+nMC = 10000                                     # number of MC/MCMC samples
+nuts = NUTS(1e-1)                               # Sampler struct
+MCint = MCMC(nMC, nuts, ρx0)
+
+# ISIntegrator
+πu = Uniform(ll, ul)                            # importance sampling: uniform distribution
+βarr = [0.02, 0.2, 1.0]                         # importance sampling: temp parameter for Gibbs biasing dist.
+πg1 = Gibbs(πgibbs0, β=βarr[1], θ=ρθ.μ)
+πg2 = Gibbs(πgibbs0, β=βarr[2], θ=ρθ.μ)
+πg3 = Gibbs(πgibbs0, β=βarr[3], θ=ρθ.μ)
+nβ = length(βarr)
