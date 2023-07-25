6ea8f2e19b52b5d851638b44db79d0a9598cf0d2
joannajzou <joannajzou@gmail.com>
2023-07-25 16:15:29 -0700

    potential function with standardized inputs

--- examples/lammps_ace_ar/qoi_meanenergy.jl
+++ examples/lammps_ace_ar/qoi_meanenergy.jl
@@ -1,14 +1,22 @@
-# define negative potential function
-V(Φ::Vector{Float64}, θ::Vector{Float64}) = -Φ' * θ # with neg sign
-
-function ∇xV(ds::DataSet, θ::Vector{Float64})
-    ∇xΦ = reduce(vcat, get_values(get_force_descriptors(ds)))
-    return vcat([-dB' * lp.β for dB in ∇xΦ])
+# define potential function
+function Vz(Φ::Vector{Float64}, z::Vector{Float64}, ρ::MvNormal)
+    θ = std_to_mvn(z, ρ)
+    return Φ' * θ
 end
 
-∇θV(Φ::Vector{Float64}, θ::Vector{Float64}) = -Φ
+function ∇xVz(ds::DataSet, z::Vector{Float64}, ρ::MvNormal)
+    θ = std_to_mvn(z, ρ)
+    ∇xΦ = reduce(vcat, get_values(get_force_descriptors(ds)))
+    return vcat([dB' * θ for dB in ∇xΦ])
+end
+
+∇zVz(Φ::Vector{Float64}, z::Vector{Float64}, ρ::MvNormal) = Φ
+
 
 # instantiate Gibbs object
+V(x, z) = Vz(x, z, πβ)
+∇xV(x, z) = ∇xVz(x, z, πβ)
+∇θV(x, z) = ∇θVz(x, z, πβ)
 πgibbs = Gibbs(V=V, ∇xV=∇xV, ∇θV=∇θV, β=1.0)
 
 # define QoI as mean energy V
