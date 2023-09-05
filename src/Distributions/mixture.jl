c96e2179a3918f3a2fd345a354d81918e609dc38
joannajzou <joannajzou@gmail.com>
2023-09-05 14:12:18 -0400

    update MixtureModel script
    - multiple constructors
    - rand() samplers with samples provided
    - normalized pdf and logpdf functions

--- src/Distributions/mixture.jl
+++ src/Distributions/mixture.jl
@@ -6,9 +6,27 @@ using StatsFuns
 
 
 # outer constructor: creates "mutable" MixtureModel with updated probabilities
-function MixtureModel(MM :: MixtureModel, prob :: Vector{<:Real})
+function MixtureModel(MM::MixtureModel,prob::Vector{<:Real})
     return MixtureModel(MM.components, prob)
 end
+
+function MixtureModel(params::Vector, π0::Gibbs) # equal weights
+    πg = [Gibbs(π0, θ=p) for p in params]
+    return MixtureModel(πg)
+end
+
+function MixtureModel(params::Vector, π0::Gibbs, prob::Vector{<:Real})
+    πg = [Gibbs(π0, θ=p) for p in params]
+    return MixtureModel(πg, prob)
+end
+
+function MixtureModel(dists::Tuple{MixtureModel, MixtureModel}, wts::Tuple{Real, Real})
+    n = length(dists)
+    comps = reduce(vcat, [dists[i].components for i = 1:n])
+    α = reduce(vcat, [wts[i] .* probs(dists[i]) for i = 1:n])
+    return MixtureModel(comps, α)
+end
+
 
 # 0 - parameters
 # function components(d::MixtureModel) already defined
@@ -21,30 +39,36 @@ end
 
 
 # 1 - random sampler
-function rand(d::MixtureModel{Union{Univariate,Multivariate}, Continuous, Gibbs}, n::Int, sampler::Sampler, ρ0::Distribution; burn=1000)
+function rand(d::MixtureModel{Union{Univariate,Multivariate}, Continuous, Gibbs},
+            n::Int,
+            sampler::Sampler,
+            ρ0::Distribution;
+            burn=1000)
+
     categories = StatsBase.sample(1:ncomponents(d), Weights(probs(d)), n)
     xsamp = Float64[]
     for i = 1:ncomponents(d)
         ni = length(findall(x -> x == i, categories))
         nsim = burn + ni
-        xi = rand(component(d, i), nsim, sampler, ρ0; burn=0.0) 
-        append!(xsamp, xi[burn+1:end])
+        xi = rand(component(d, i), nsim, sampler, ρ0; burn=0) 
+        append!(xsamp, rand(xi[(burn+1):end], ni))
     end
     return xsamp
 end 
 
 # samples provided
-function rand(d::MixtureModel{Union{Univariate,Multivariate}, Continuous, Gibbs}, n::Int, samples::Vector; wts::Vector=ones(ncomponents(d))/ncomponents(d))
-    d = MixtureModel(d, wts)
-    ntot = length(samples[1])
+function rand(d::MixtureModel{Union{Univariate,Multivariate}, Continuous, Gibbs}, 
+            n::Int,
+            samples::Vector)
     categories = StatsBase.sample(1:ncomponents(d), Weights(probs(d)), n)
-    xsamp = Vector{Float64}[]
+    xsamp = Float64[]
     catratio = Float64[]
     for i = 1:ncomponents(d)
+        ntot = length(samples[i])
         ni = length(findall(x -> x == i, categories))
         append!(catratio, ni/n)
         if ni != 0
-            randid = StatsBase.sample(1:ntot, ni; replace=false)
+            randid = StatsBase.sample(1:ntot, ni; replace=true)
             xi = samples[i][randid]
             append!(xsamp, xi)
         end
@@ -52,21 +76,31 @@ function rand(d::MixtureModel{Union{Univariate,Multivariate}, Continuous, Gibbs}
     return xsamp, catratio
 end 
 
-# 2 - unnormalized pdf
-function updf(d::MixtureModel{Union{Univariate,Multivariate}, Continuous, Gibbs}, x)
+# 2 - pdf
+function pdf(d::MixtureModel{Union{Univariate,Multivariate}, Continuous, Gibbs}, x::Float64, integrator::Integrator)
     p = probs(d)
-    return sum(pi * updf(component(d, i), x) for (i, pi) in enumerate(p) if !iszero(pi))
+    return sum(p_i * pdf(component(d, i), x, integrator) for (i, p_i) in enumerate(p) if !iszero(p_i))
 end 
 
-# 3 - log unnormalized pdf
-function logupdf(d::MixtureModel{Union{Univariate,Multivariate}, Continuous, Gibbs}, x)
+function pdf(d::MixtureModel{Union{Univariate,Multivariate}, Continuous, Gibbs}, xsamp::Vector{Float64}, integrator::Integrator)
     p = probs(d)
-    lp = logsumexp(log(pi) + logupdf(component(d, i), x) for (i, pi) in enumerate(p) if !iszero(pi))
+    return sum(p_i * updf.((component(d, i),), xsamp) ./ normconst(component(d, i), integrator) for (i, p_i) in enumerate(p) if !iszero(p_i))
+end 
+
+
+# 3 - log unnormalized pdf
+function logpdf(d::MixtureModel{Union{Univariate,Multivariate}, Continuous, Gibbs}, x::Float64, integrator::Integrator)
+    p = probs(d)
+    lp = logsumexp(log(p_i) + logpdf(component(d, i), x, integrator) for (i, p_i) in enumerate(p) if !iszero(p_i))
     return lp
 end
 
-# 4 - normalization constant (partition function)
-function normconst(d::MixtureModel{Union{Univariate,Multivariate}, Continuous, Gibbs}, ξ::Vector, w::Vector)
-    return sum(w' * updf.((d,), ξ))
+function logpdf(d::MixtureModel{Union{Univariate,Multivariate}, Continuous, Gibbs}, xsamp::Vector{Float64}, integrator::Integrator)
+    p = probs(d)
+    data = reduce(hcat, [log(p_i) .+ logupdf.((component(d, i),), xsamp) .- log(normconst(component(d, i), integrator)) for (i, p_i) in enumerate(p) if !iszero(p_i)])
+    lp = logsumexp.([data[i,:] for i = 1:size(data,1)])
+    return lp
 end
+
+
 
