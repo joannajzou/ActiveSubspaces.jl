102561f28d889f997ba6d061137501ac3b6b3236
joannajzou <joannajzou@gmail.com>
2023-09-06 16:12:57 -0400

    update adaptive_is utils

--- examples/doublewell1D/adaptive_is_utils.jl
+++ examples/doublewell1D/adaptive_is_utils.jl
@@ -154,15 +154,18 @@ using Optim
 # end
 
 
-function update_dist_params(λsamp::Vector, πg::Gibbs, qois::Vector{GibbsQoI}, integrator::ISSamples)
+function update_dist_params(λsamp::Vector,
+                        πg::Gibbs,
+                        qois::Vector{GibbsQoI},
+                        integrator::ISSamples)
 
     xsamp = integrator.xsamp
     N = length(xsamp)
     J = length(λsamp)
 
     # define functions
-    # Zg(λ) = 1/N * sum(g.(xsamp, (λ,)) ./ h.(xsamp))
-    Zg_quad(λ) = normconst(Gibbs(πg, θ=λ), GQint)
+    # Zg(λ) = 1/N * sum(g.(xsamp, (λ,)) ./ h.(xsamp)) # Monte Carlo estimate
+    Zg(λ) = normconst(Gibbs(πg, θ=λ), GQint)
     g(x, λ) = updf(Gibbs(πg, θ=λ), x)
 
     # initilaize
@@ -175,10 +178,10 @@ function update_dist_params(λsamp::Vector, πg::Gibbs, qois::Vector{GibbsQoI}, 
         function ϕj(xsamp, λ)
             hsamp = qj.h.(xsamp)
             hsamp = [abs.(h) for h in hsamp]
-            return hsamp .* log.(g.(xsamp, (λ,)) ./ Zg_quad(λ))
+            return hsamp .* log.(g.(xsamp, (λ,)) ./ Zg(λ))
         end
 
-        logwt(x) = logupdf.((qj.p,), x) - logpdf(integrator.g, x, GQint)
+        logwt(xv) = logupdf.((qj.p,), xv) - logpdf(integrator.g, xv, GQint)
         
         # estimators
         M = maximum(logwt(xsamp))
@@ -192,7 +195,7 @@ function update_dist_params(λsamp::Vector, πg::Gibbs, qois::Vector{GibbsQoI}, 
         end
 
         @time res = optimize(entlog, λsamp[j], NelderMead(),
-                 Optim.Options(f_tol=1e-3, g_tol=1e-6, f_calls_limit=100)) # ; autodiff = :forward)
+                 Optim.Options(show_trace=true, f_tol=1e-4, g_tol=1e-6, f_calls_limit=100)) # ; autodiff = :forward)
 
         λopt[j] = Optim.minimizer(res)
     end
@@ -233,29 +236,37 @@ end
 # end
 
 
-function update_mixture_wts(αsamp::Vector, λsamp::Vector, πg::Gibbs, qois::Vector{GibbsQoI}, integrator::ISSamples)
+function update_mixture_wts(αsamp::Vector,
+                        λsamp::Vector,
+                        πg::Gibbs,
+                        qois::Vector{GibbsQoI},
+                        integrator::ISSamples)
     xsamp = integrator.xsamp
     N = length(xsamp)
     J = length(λsamp)
 
     ϕj(x,j) = qois[j].h(x)
     πfj(j) = qois[j].p
+    # πgj(j) = Gibbs(πg, θ=λsamp[j])
+    # numj(xv,j) = ϕj.(xv,(j,)) .* updf.((πfj(j),), xv) # .- βsamp[j] * updf.((πgj(j),), xv)
+    function numj2(xv,j)
+        numj(x) = ϕj(x,j) * updf(πfj(j), x)
+        return [numj(x)'*numj(x) for x in xv]
+    end
 
-    comps = [Gibbs(πg, θ=λ) for λ = λsamp]
+    comps = [Gibbs(πg, θ=λ) for λ in λsamp]
     πgα(α) = MixtureModel(comps, α)
-
-    logwt(x,α,j) = 2*logupdf.((πfj(j),), x) - logpdf(πgα(α), x, GQint) - logpdf(integrator.g, x, GQint)
-
+    denomj(xv, α) = pdf(πgα(α), xv, GQint) .* pdf(integrator.g, xv, GQint)
+    
     function varj(α,j)
         α = normalize(α)
-        M = maximum(logwt(xsamp, α, j))
-        return sum( ϕj.(xsamp,(j,))'*ϕj.(xsamp,(j,)) .* exp.(logwt(xsamp, α, j) .- M) ) / sum( exp.(logwt(xsamp, α, j) .- M) )
+        return sum( numj2(xsamp,j) ./ denomj(xsamp,α) )
     end
 
     varsum(α) = sum([varj(α, j) for j = 1:J])
-
+ 
     @time res = optimize(varsum, αsamp, NelderMead(),
-                 Optim.Options(show_trace=true, f_tol=1e-2, g_tol=1e-2, f_calls_limit=50))
+                 Optim.Options(show_trace=true, f_tol=1e-3, g_tol=1e-2, f_calls_limit=1000))
 
     return normalize(Optim.minimizer(res))
 end
@@ -294,29 +305,29 @@ end
 
 
 
+#### KEEP?
+# colscheme = [RGB((i-1)*0.7/niter, 0.3 + (i-1)*0.7/niter, 0.6 + (i-1)*0.2/niter) for i = 1:niter] # color scheme for model 1
+# figs = Vector{Figure}(undef, nλ)
+# for j = 1:nλ
+#     with_theme(custom_theme) do
+#         figs[j] = Figure(resolution = (600, 600))
+#         ax = Axis(figs[j][1, 1],  xlabel="x", ylabel="ϕ(x) π(x)")
+#         # target: integrand
+#         lines!(ax, xplot, integrand(xplot, λsamp[j], V, πgibbs0, GQint), linewidth=2, color=:black, label="target")
+#         # original distribution
+#         g0 = Gibbs(πgibbs0, β=0.5, θ=λsamp[j])
+#         lines!(ax, xplot, updf.((g0,), xplot) ./ normconst(g0, GQint), color=:red, label="original")
+#         # adapted distribution
+#         for k = 2:(niter-1)
+#             gk = Gibbs(πgibbs0, β=0.5, θ=λset[k][j])
+#             lines!(ax, xplot, updf.((gk,), xplot) ./ normconst(gk, GQint), color=colscheme[k])
+#         end
+#         gk = Gibbs(πgibbs0, β=0.5, θ=λset[end][j])
+#         lines!(ax, xplot, updf.((gk,), xplot) ./ normconst(gk, GQint), color=colscheme[end], label="adapted")
 
-colscheme = [RGB((i-1)*0.7/niter, 0.3 + (i-1)*0.7/niter, 0.6 + (i-1)*0.2/niter) for i = 1:niter] # color scheme for model 1
-figs = Vector{Figure}(undef, nλ)
-for j = 1:nλ
-    with_theme(custom_theme) do
-        figs[j] = Figure(resolution = (600, 600))
-        ax = Axis(figs[j][1, 1],  xlabel="x", ylabel="ϕ(x) π(x)")
-        # target: integrand
-        lines!(ax, xplot, integrand(xplot, λsamp[j], V, πgibbs0, GQint), linewidth=2, color=:black, label="target")
-        # original distribution
-        g0 = Gibbs(πgibbs0, β=0.5, θ=λsamp[j])
-        lines!(ax, xplot, updf.((g0,), xplot) ./ normconst(g0, GQint), color=:red, label="original")
-        # adapted distribution
-        for k = 2:(niter-1)
-            gk = Gibbs(πgibbs0, β=0.5, θ=λset[k][j])
-            lines!(ax, xplot, updf.((gk,), xplot) ./ normconst(gk, GQint), color=colscheme[k])
-        end
-        gk = Gibbs(πgibbs0, β=0.5, θ=λset[end][j])
-        lines!(ax, xplot, updf.((gk,), xplot) ./ normconst(gk, GQint), color=colscheme[end], label="adapted")
-
-        axislegend(ax)
-    end
-end
+#         axislegend(ax)
+#     end
+# end
 
 
 
@@ -339,7 +350,12 @@ end
 
 
 
-function adapt_mixture_biasing_dist(λsamp::Vector, q::GibbsQoI, πg::Gibbs; N0=5000, Nk=N0, niter=5)
+function adapt_mixture_biasing_dist_with_wts(λsamp::Vector,
+                    q::GibbsQoI,
+                    πg::Gibbs;
+                    N0=5000,
+                    Nk=N0,
+                    niter=10)
 
     nλ = length(λsamp)
 
@@ -372,7 +388,7 @@ function adapt_mixture_biasing_dist(λsamp::Vector, q::GibbsQoI, πg::Gibbs; N0=
         println("updating params")
         λk = update_dist_params(λset[k-1], πg, qois, ISint)
         # println("updating mixture weights")
-        # αk = update_mixture_wts(αset[k-1], λset[k-1], πg, qois, ISint)
+        αk = update_mixture_wts(αset[k-1], λset[k-1], πg, qois, ISint)
         # compute new biasing dist.
         gk = MixtureModel(λk, πg, αk)
         # draw samples
@@ -386,9 +402,70 @@ function adapt_mixture_biasing_dist(λsamp::Vector, q::GibbsQoI, πg::Gibbs; N0=
     
         # save values
         λset[k] = λk
-        # αset[k] = αk
+        αset[k] = αk
         h0 = hk
     end
 
     return λset, αset
+end
+
+
+
+function adapt_mixture_biasing_dist(λsamp::Vector,
+            q::GibbsQoI,
+            πg::Gibbs;
+            N0=5000,
+            Nk=N0,
+            niter=10)
+
+    nλ = length(λsamp)
+
+    # define target expectations 
+    θsamp = λsamp
+    qois = [assign_param(q, θ) for θ in θsamp]
+
+    # initialize vectors
+    λset = Vector{Vector}(undef, niter)
+    αset = Vector{Vector{Float64}}(undef, niter)
+    xset = []
+
+    ## compute initial biasing dist
+    λset[1] = λsamp 
+    αset[1] = 1/nλ * ones(nλ)
+    αk = αset[1]
+    h0 = MixtureModel(λset[1], πg, αset[1])
+
+    # draw samples
+    xsamp = rand(h0, N0, nuts, ρx0)
+    ISint = ISSamples(h0, xsamp)
+    append!(xset, xsamp)
+
+    Neval = N0
+
+    # start adaptation 
+    for k = 2:niter
+        println("------- iteration $k -------")
+        # update parameters
+        println("updating params")
+        λk = update_dist_params(λset[k-1], πg, qois, ISint)
+        # println("updating mixture weights")
+        # αk = update_mixture_wts(αset[k-1], λset[k-1], πg, qois, ISint)
+        # compute new biasing dist.
+        gk = MixtureModel(λk, πg, αk)
+        # draw samples
+        xsamp = rand(gk, Nk, nuts, ρx0)
+        append!(xset, xsamp)
+        # update biasing distribution
+        Neval += Nk
+        hk = MixtureModel((h0, gk), ((Neval - Nk)/Neval, Nk/Neval))
+        # check stopping criterion
+        # println(meet_stop_crit())
+
+        # save values
+        λset[k] = λk
+        # αset[k] = αk
+        h0 = hk
+    end
+
+    return λset # , αset
 end
